f43c4eab5c9c56fff1000baeef97ae847f35714c
Xaobin <40346917+Xaobin@users.noreply.github.com>
2023-04-27 23:38:36 -0400

    Update code9.ts

--- parte1/code9.ts
+++ parte1/code9.ts
@@ -2,7 +2,12 @@
 // ==> Exemplo 01: Numeric Enums (Enums Numérico)
 //https://www.typescriptlang.org/docs/handbook/2/everyday-types.html#enums
 /*
- Enums são um recurso adicionado ao JavaScript pelo TypeScript que permite descrever um valor que pode ser um de um conjunto de possíveis constantes nomeadas.  Ao contrário da maioria dos recursos do TypeScript, essa não é uma adição de nível de tipo ao JavaScript, mas algo adicionado à linguagem e ao tempo de execução.  Por causa disso, é um recurso que você deve saber que existe, mas talvez adie o uso, a menos que tenha certeza.  Você pode ler mais sobre enums na página de referência do Enum:
+ Enums são um recurso adicionado ao JavaScript pelo TypeScript que permite descrever
+ um valor que pode ser um de um conjunto de possíveis constantes nomeadas.  Ao contrário
+ da maioria dos recursos do TypeScript, essa não é uma adição de nível de tipo ao 
+ JavaScript, mas algo adicionado à linguagem e ao tempo de execução.  Por causa disso, 
+ é um recurso que você deve saber que existe, mas talvez adie o uso, a menos que 
+ tenha certeza.  Você pode ler mais sobre enums na página de referência do Enum:
  https://www.typescriptlang.org/docs/handbook/enums.html
  * 
  * 
