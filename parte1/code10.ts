77c00b46fb6e5a00fee745f1325dc45b08ef974f
Xaobin <40346917+Xaobin@users.noreply.github.com>
2023-04-29 01:13:42 -0400

    Update code10.ts

--- parte1/code10.ts
+++ parte1/code10.ts
@@ -1,9 +1,12 @@
 //APRENDIZADO TYPESCRIPT - ANY
 //https://www.typescriptlang.org/docs/handbook/2/everyday-types.html#any
 /*
- * O TypeScript também tem um tipo especial, any, que você pode usar sempre que não quiser que um valor específico cause erros de verificação de tipo.
+ * O TypeScript também tem um tipo especial, any, que você pode usar sempre que
+ não quiser que um valor específico cause erros de verificação de tipo.
 
- Quando um valor é do tipo any, você pode acessar qualquer propriedade dele (que por sua vez será do tipo any), chamá-lo como uma função, atribuí-lo a (ou de) um valor de qualquer tipo ou praticamente qualquer outra coisa
+ Quando um valor é do tipo any, você pode acessar qualquer propriedade dele
+ (que por sua vez será do tipo any), chamá-lo como uma função, atribuí-lo a (ou de)
+ um valor de qualquer tipo ou praticamente qualquer outra coisa
  */
 // ==> Exemplo 01: Tipo Any
 const a: any = 44;
