af9deffc9a4ea4b9068eda4af400b5784f958c4d
Xaobin <40346917+Xaobin@users.noreply.github.com>
2024-11-05 12:29:09 -0400

    Atualizar o code5.ts

--- parte1/code5.ts
+++ parte1/code5.ts
@@ -3,7 +3,7 @@
 // - BigInt literals are not available when targeting lower than ES2020 
 // - Resolution
 // - tsc code5.ts --target es2020
-//=========================================
+//============================
 let num1: number = 23.0;
 let num2: number = 0x78CF;
 let num3: number = 0o577;
@@ -12,7 +12,7 @@ console.log('Number - Ponto Flutuante...: ', num1);
 console.log('Number - Hexadecimal...: ', num2);
 console.log('Number - Octal...: ', num3);
 console.log('Number - Binário...: ', num4);
-//=========================================
+//===========================
 let big1: bigint = 9007199254740991n;
 let big2: bigint = 0b100000000000000000000000000000000000000000000000000011n;;
 let big3: bigint = 0x20000000000003n;
@@ -21,4 +21,4 @@ console.log('Bigint...: ', big1);
 console.log('Bigint - Binário...: ', big2);
 console.log('Bigint - Hexadecimal...: ', big3);
 console.log('Bigint - Octal...: ', big4);
-//=========================================
+//=============================
